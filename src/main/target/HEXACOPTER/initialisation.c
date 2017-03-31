d52615bab67a91af6a8461be5d91da3b989d275a
Robin van der Gracht <robinvandergracht@gmail.com>
2017-03-31 13:13:26 +0200

    targets: hexacopter: added hex_leds module
    Signed-off-by: Robin van der Gracht <robinvandergracht@gmail.com>

--- src/main/target/HEXACOPTER/initialisation.c
+++ src/main/target/HEXACOPTER/initialisation.c
@@ -1,9 +1,27 @@
 
 #include "platform.h"
 
+#include "drivers/io.h"
+
+#include "hex_leds.h"
+
 void targetPreInit(void)
 {
 	//FIXME Enable 5V here
 
 	GPIO_PinRemapConfig(GPIO_Remap_SPI1, ENABLE);
+
+	for (int i = 0; i < 6; i++) {
+		IO_t io = IOGetByTag(DEFIO_TAG_MAKE(DEFIO_GPIOID__C, i));
+		IOInit(io, OWNER_LED, RESOURCE_INDEX(i));
+		IOConfigGPIO(io, IOCFG_OUT_PP);
+		IOLo(io);
+	}
+
+	HEX_LED1_OFF;
+	HEX_LED2_OFF;
+	HEX_LED3_OFF;
+	HEX_LED4_OFF;
+	HEX_LED5_OFF;
+	HEX_LED6_OFF;
 }
