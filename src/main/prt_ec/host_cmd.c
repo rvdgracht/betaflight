1c3e6d5e04739145815b3e4f6416ad1014377266
Robin van der Gracht <robinvandergracht@gmail.com>
2017-04-13 22:23:03 +0200

    prt_ec: removed debug info
    Signed-off-by: Robin van der Gracht <robinvandergracht@gmail.com>

--- src/main/prt_ec/host_cmd.c
+++ src/main/prt_ec/host_cmd.c
@@ -6,8 +6,6 @@
 
 #include "host_cmd.h"
 
-#include "hex_leds.h"
-
 enum host_cmd_state {
 	HOST_CMD_READY = 0,
 	HOST_CMD_RECEIVED,
@@ -34,19 +32,12 @@ int host_pkt_recieved(struct host_packet *host_pkt)
 		return -1;
 	}
 
-	if (pkt->id >= 200) {
-		/* Responce handling is not (yet) implemented */
-		pkt->id = EC_RES_ERROR;
-		return -1;
-	}
-
 	if (pkt->crc != crc16((uint8_t *)pkt, sizeof(*pkt) - 2)) {
 		/* Packet has invalid crc */
 		pkt->id = EC_RES_INVALID_CHECKSUM;
 		return -1;
 	}
 
-	HEX_LED3_TOGGLE;
 	active_pkt = host_pkt;
 	state = HOST_CMD_RECEIVED;
 	return 0;
@@ -67,7 +58,6 @@ static const struct host_command *find_host_command(int command)
 	const struct host_command *cmd;
 
 	for (cmd = __hcmds_start; cmd < __hcmds_end; cmd++) {
-		HEX_LED5_TOGGLE;
 		if (command == cmd->command)
 			return cmd;
 	}
@@ -83,7 +73,6 @@ static enum prt_ec_result host_cmd_process(void)
 	if (!cmd)
 		return EC_RES_INVALID_COMMAND;
 
-	HEX_LED5_TOGGLE;
 	return cmd->handler(pkt->data);
 }
 
@@ -104,7 +93,6 @@ void host_cmd_task_handler(timeUs_t currentTimeUs)
 	state = HOST_CMD_PROCESSING;
 
 	pkt->id = host_cmd_process();
-	HEX_LED4_TOGGLE;
 
 //	state = HOST_CMD_PROCESSED;
 	state = HOST_CMD_READY;
