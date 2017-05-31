350749710dae7884342814d6f6b5d694b33d0506
Robin van der Gracht <robinvandergracht@gmail.com>
2017-05-31 20:57:53 +0200

    target: hexacopter: Use standard pwm protocol by default
    Signed-off-by: Robin van der Gracht <robinvandergracht@gmail.com>

--- src/main/target/HEXACOPTER/config.c
+++ src/main/target/HEXACOPTER/config.c
@@ -6,6 +6,7 @@
 #ifdef TARGET_CONFIG
 
 #include "common/axis.h"
+#include "drivers/pwm_output.h"
 #include "sensors/compass.h"
 
 #include "config/config_master.h"
@@ -13,5 +14,6 @@
 void targetConfiguration(master_t *config)
 {
 	config->compassConfig.mag_hardware = MAG_AK8975;
+	config->motorConfig.motorPwmProtocol = PWM_TYPE_STANDARD;
 }
 #endif
