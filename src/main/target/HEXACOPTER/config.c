3fa8ae64db5cdb9a9fbde70b83023706c092ae5a
Robin van der Gracht <robinvandergracht@gmail.com>
2017-06-14 15:59:33 +0200

    target: hexacopter: Setup failsafe landing
    Signed-off-by: Robin van der Gracht <robinvandergracht@gmail.com>

--- src/main/target/HEXACOPTER/config.c
+++ src/main/target/HEXACOPTER/config.c
@@ -7,6 +7,7 @@
 
 #include "common/axis.h"
 #include "drivers/pwm_output.h"
+#include "flight/failsafe.h"
 #include "sensors/compass.h"
 
 #include "config/config_master.h"
@@ -15,5 +16,10 @@ void targetConfiguration(master_t *config)
 {
 	config->compassConfig.mag_hardware = MAG_AK8975;
 	config->motorConfig.motorPwmProtocol = PWM_TYPE_STANDARD;
+
+	/* Failsafe, 10 seconds to land or regain control */
+	config->failsafeConfig.failsafe_off_delay = 100;
+	config->failsafeConfig.failsafe_procedure = FAILSAFE_PROCEDURE_AUTO_LANDING;
+	config->failsafeConfig.failsafe_throttle = 1500;
 }
 #endif
